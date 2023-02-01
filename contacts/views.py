e4802e0c53e2e923f58a027e9db90bc97ee84185
Jonibek Sayfullaev <jony@Jonibeks-MacBook-Air.local>
2023-02-01 22:03:09 +0900

    deployment

---
+++ contacts/views.py
@@ -0,0 +1,46 @@
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import Contact
+from django.core.mail import send_mail
+
+
+# Create your views here.
+
+def contact(request):
+    if request.method == "POST":
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        # Check if user made inquery already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(
+                    request, "You have already made an inquiry for this listing")
+                return redirect('/listings/' + listing_id)
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone,
+                          message=message, user_id=user_id)
+
+        contact.save()
+        send_mail(
+            'Property listing inquery',
+            'There has been inquery for ' + listing +
+            '. Sign into for getting more info.',
+            'jonysayfullaev2000@example.com',
+            [realtor_email, 'jonicks2000@gmail.com'],
+            fail_silently=False,
+
+        )
+
+        messages.success(
+            request, "Your request has been submitted, a realtor get back to you soon")
+        return redirect('/listings/' + listing_id)
---
+++ listings/choices.py
@@ -0,0 +1,48 @@
+bedroom_choices = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+}
+
+price_choices = {
+    '50000': '$50,000',
+    '100000': '$100,000',
+    '150000': '$150,000',
+    '200000': '$200,000',
+    '250000': '$250,000',
+    '300000': '$300,000',
+    '350000': '$350,000',
+    '400000': '$400,000',
+    '450000': '$450,000',
+    '500000': '$500,000',
+    '600000': '$600,000',
+    '700000': '$700,000',
+    '800000': '$800,000',
+    '900000': '$900,000',
+}
+
+state_choices = {
+    "Tashkent": 'Tashkent',
+    "Bektemir": 'Bektemir',
+    "Chilonzor": 'Chilonzor',
+    "Yunsobod": "Yunsobod",
+    "Mirzo-Ulugbek": "Mirzo-Ulugbek",
+    "Sergeli": "Sergeli",
+    "Shayxontoxur": "Shayxontoxur",
+    "Olmozor": "Olmozor",
+    "Uchtepa": "Uchtepa",
+    "Yakkasaray": "Yakkasaray",
+    "Yunusobod": "Yunusobod",
+    "Yangihayot": "Yangihayot",
+    "Shoro": "Shoro",
+    "Mirabad": "Mirabad",
+    "M.Fozilov": "M.Fozilov",
+    "Kibray": "Kibray"
+}
