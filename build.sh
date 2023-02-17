524a2eaa0916e455790b20ea3c20c3923823ca00
Jonibek Sayfullaev <jony@Jonibeks-MacBook-Air.local>
2023-02-17 18:07:30 +0900

    bug fixed

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+poetry install
+python manage.py collectstatic --no-input
+python manage.py migrate
